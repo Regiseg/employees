15ba1bfd89cdba5787d8741bba9f38d17a7485a1
Bibi <huitzilopochtlik@gmail.com>
2021-06-03 16:15:37 +0200

    Employee mintapélda

--- src/main/resources/db/migration/V01__init.sql
+++ src/main/resources/db/migration/V01__init.sql
@@ -1,1 +1,1 @@
-create table employees (id bigint not null auto_increment, emp_name varchar(255), primary key (id));
+create table if not exists employees (id bigint not null auto_increment, emp_name varchar(255), primary key (id));
